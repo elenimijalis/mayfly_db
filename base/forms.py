0221dc444358246a2232dd1930b138e060ce71b4
Eleni Mijalis <elenimijalis@gmail.com>
2015-11-13 22:42:46 -0600

    added more templating

--- base/forms.py
+++ base/forms.py
@@ -4,5 +4,4 @@ class PaperForm(forms.Form):
     title = forms.CharField(label='Title', required=False)
     author = forms.CharField(label='Author', required=False)
     journal = forms.CharField(label='Journal', required=False)
-    year_start = forms.IntegerField(label='', required=False)
-    year_end = forms.IntegerField(label='', required=False)
+    date = forms.IntegerField(label='Year', required=False)
--- base/views.py
+++ base/views.py
@@ -10,12 +10,14 @@ def get_title(request):
 
     if 'title' in data and len(data['title']) > 0:
         papers = papers.filter(title__icontains=data['title'])
+    else:
+        return render(request, 'base/search.html', {'form': form})
 
     if 'author' in data and len(data['author']) > 0:
         papers = papers.filter(author__icontains=data['author'])
 
     if 'journal' in data and len(data['journal']) > 0:
-        papers = papers.filter(journal__icontains=data['journal'])
+        papers = papers.filter(journal__name__icontains=data['journal'])
 
     # if 'year_start' in data and len(data['year_start']) > 0:
         # papers = papers.filter(title__icontains=data['title'])
